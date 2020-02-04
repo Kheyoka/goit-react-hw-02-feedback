df7cdfbdad78dd1d8e70afac4e550d44846b5bf5
Kseniya G Vo <vwk8888@gmail.com>
2020-02-04 20:57:43 +0200

    Updates

---
+++ precache-manifest.92e621ede51e27638d51a603af49c3ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b2c3faf9cf5eca1b69faaa9cc36bce",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "827293d70877a3a67687",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e25a497e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e25a497e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521ad10c92b9eeb82003",
+    "url": "/goit-react-hw-02-feedback/static/js/main.89afeae2.chunk.js"
+  },
+  {
+    "revision": "123d81bea882563839a7",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d87da01c.js"
+  }
+]);
